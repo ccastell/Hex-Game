cf613900642f8f50fa546e7b2eb6d7636d5591a5
carlcastello <carl.castello96@gmail.com>
2020-11-21 00:51:03 -0700

    Button clicked are now implemented on individual views

--- view/constant.py
+++ view/constant.py
@@ -1,6 +1,22 @@
+from typing import Tuple
 MAIN_FONT: str = 'resources/fonts/OpenSans_Regular.ttf'
 TITLE_FONT: str = 'resources/fonts/ComicSans_Regular.ttf'
 
+
+BLACK: Tuple[int, int, int] = (0, 0, 0)
+WHITE: Tuple[int, int, int] = (255, 255, 255)
+
+MAIN_BACKGROUND_COLOR: Tuple[int, int, int] = (246, 246, 246)
+
+BUTTON_BACKGROUND_COLOR: Tuple[int, int, int] = (215, 58, 73)
+
+MAIN_TEXT_COLOR: Tuple[int, int, int] = (32, 74, 135)
+SUB_TEXT_COLOR: Tuple[int, int, int] = (143, 89, 2)
+
+LARGE_PADDING = 15
+MEDIUM_PADDING = 10
+SMALL_PADDING = 5
+
 LARGE_TEXT: int = 80
-MEDIUM_TEXT: int = 50
-SMALL_TEXT: int = 20
+MEDIUM_TEXT: int = 30
+SMALL_TEXT: int = 18
--- view/screen_view.py
+++ view/screen_view.py
@@ -1,12 +1,26 @@
+import enum
+from typing import Tuple, List
 
+from pygame import Surface
+
+from view.button_view import ButtonView
 
 class ScreenView:
     
-    _screen = None
+    _screen: Surface
+    _width: float
+    _height: float
+    _center: Tuple[float, float]
 
-    def __init__(self, screen, size):
+    def __init__(self, screen: Surface, size: int):
         self._screen = screen   
         self._width, self._height = size
+        self._center = (self._width // 2, self._height // 2)
 
     def update(self):
         raise NotImplementedError
+
+    def _find_button(self, mouse: Tuple[int, int], buttons: List[ButtonView]) -> enum.Enum:
+        for button in buttons:
+            if button.collidepoint(mouse):
+                return button.id()
