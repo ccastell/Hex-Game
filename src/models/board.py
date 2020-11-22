ff6de3b3db88b5dc56938521510653f76f6fd745
carlcastello <carl.castello96@gmail.com>
2020-11-22 13:05:46 -0700

    Static Board Size and row/column fetching and name changes

--- src/models/board.py
+++ src/models/board.py
@@ -1,44 +1,46 @@
 from typing import Tuple
 
-from tile import Tile
+from src.models.tile import Tile
 
 class Board:
 
-    __size: int
+    _size: int
 
-    __matrix: Tuple[Tuple[Tile , ...], ...] = []
+    _matrix: Tuple[Tuple[Tile , ...], ...] = []
 
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self):
+        self._size = 11
 
-
-        def __neighbors(column_index, row_index):
+        def _neighbors(column_index, row_index):
             return [
                 (column_index, row_index - 1), (column_index + 1, row_index - 1),
                 (column_index - 1, row_index), (column_index + 1, row_index), 
                 (column_index - 1, row_index + 1), (column_index, row_index + 1)
             ]
 
-        for row_index in range(0, self.__size):
-            column = []
-            for column_index in range(0, self.__size):
-                column.append(
+        for row_index in range(0, self._size):
+            row = []
+            for column_index in range(0, self._size):
+                row.append(
                     Tile(
-                        (column_index, row_index - 1),
+                        (column_index, row_index),
                         list(filter(
                             lambda neighbor: False if neighbor[0] < 0 or neighbor[1] < 0 else True,
-                            __neighbors(column_index, row_index)
+                            _neighbors(column_index, row_index)
                         ))
                     )
                 )
+            self._matrix.append(row)
 
-    def matrix(self):
-        return self.__matrix
+    def matrix(self) -> Tuple[Tuple[Tile , ...], ...]:
+        return self._matrix
+
+    def size(self) -> int:
+        return self._size
+
+    def find_tile(self, x, y) -> Tile:
+        return self._matrix[y][x]
 
     def check_win(self):
         # TODO: Check the win condition
         raise NotImplementedError
-
-if __name__ == "__main__":
-    board = Board(11)
-    print(board.matrix())
