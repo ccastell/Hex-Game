598f903b0719004698d47947ec70f4e74b0c8b87
carlcastello <carl.castello96@gmail.com>
2020-11-21 17:21:55 -0700

    Image rendering of tile view

--- src/views/tile_view.py
+++ src/views/tile_view.py
@@ -3,8 +3,11 @@ import enum
 from typing import Tuple, Dict
 
 from pygame import Rect, Surface
+from pygame.image import load
 from pygame.draw import rect
 
+from src.models.tile import Tile, State as TileState
+from src.controllers.tile_controller import TileController
 
 class Point(enum.Enum):
     TOP_RIGHT: int = 1
@@ -20,20 +23,46 @@ class TileView:
     _screen: Surface
     _center: Tuple[float, float]
 
+    _tile: Tile
+    _tile_controler: TileController
+
     _tile_container: Rect
+
+    _tile_hexagon_location: str = 'resources/images/hexagon'
 
     def __init__(self,
                  screen: Surface,
-                 center: Tuple[float, float],
-                 size: int = 10):
+                 tile: Tile,
+                 center: Tuple[float, float]):
+
         self._screen = screen
         self._center = center
 
+        self._tile = tile
+        self._tile_controler = TileController(tile)
 
         self._draw_tile()
 
+    def _hexagon_image(self):
+
+        tile_state = self._tile.state()
+
+        def _image_source():
+            if tile_state == TileState.BLUE:
+                return f'{self._tile_hexagon_location}/blue.png'
+            elif tile_state == TileState.RED:
+                return f'{self._tile_hexagon_location}/red.png'
+            else:
+                return f'{self._tile_hexagon_location}/white.png'
+
+        return load(_image_source())
+
     def _draw_tile(self):
-        self._tile_container = rect(
-            self._screen,
-            self._background_color
+        hexagon_image = self._hexagon_image()
+        
+        image_rect: Rect = hexagon_image.get_rect()
+        image_rect.center = self._center
+        self._screen.blit(
+            hexagon_image,
+            image_rect
         )
