a23e27edabb39a262f1076ba04fc48417ef30af6
carlcastello <carl.castello96@gmail.com>
2020-11-21 17:21:00 -0700

    Changed the names-space of fields

--- src/models/tile.py
+++ src/models/tile.py
@@ -5,32 +5,33 @@ from typing import Tuple, List
 
 class State(enum.Enum):
     WHITE: int = 0
-    RED: int = 0
-    BLUE: int = 0
+    RED: int = 1
+    BLUE: int = 2
 
 class Tile:
 
-    __center: Tuple[int, int]
+    _id: Tuple[int, int]
 
-    __neighbors: List[Tuple[int, int]]
+    _neighbors: List[Tuple[int, int]]
 
-    __state: State = State.WHITE
+    _state: State = State.WHITE
 
-    def __init__(self, center: Tuple[int, int], neighbors: List[Tuple[str, int]]):
-        self.__center = center
-        self.__neighbors = neighbors
+    def __init__(self, id: Tuple[int, int], neighbors: List[Tuple[str, int]]):
+        self._id = id
+        self._neighbors = neighbors
+        self._state = State.BLUE
 
-    def center(self) -> Tuple[int, int]:
-        return self.__center
+    def id(self) -> Tuple[int, int]:
+        return self._id
 
     def neighbors(self) -> List[Tuple[str, int]]:
-        return self.__neighbors
+        return self._neighbors
 
     def state(self) -> State:
-        return self.__state
+        return self._state
 
     def update_state(self, new_state) -> None:
         self.__state = new_state
 
     def __str__(self):
-        return f'{self.__center[0]}{str(self.__center[1])}'
+        return f'{self._id[0]}{str(self._id[1])}'
