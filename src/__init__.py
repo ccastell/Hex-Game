e8aa7cf48b922fdb56fae0e4fa294aab3b6e2d82
carlcastello <carl.castello96@gmail.com>
2020-11-23 15:38:20 -0700

    Included player in the game

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,14 @@
 
 from src.views import Hex
 from src.models.board import Board
+from src.models.player import Player, Order
+
 
 def start_game():
 
     board_model: Board = Board()
+    player_1: Player = Player(Order.FIRST)
+    player_2: Player = Player(Order.SECOND)
 
     game = Hex(board_model)
-    game.on_execute()
+    game.on_execute(player_1, player_2)
