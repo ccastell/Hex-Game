034c13d2350c5229a629eb50cc4358f41ad0b869
carlcastello <carl.castello96@gmail.com>
2020-11-23 15:52:45 -0700

    Player, Undo, Swap functionality and some button refactor

--- src/views/board_view.py
+++ src/views/board_view.py
@@ -1,25 +1,23 @@
 import enum
-from typing import Tuple
+from typing import Tuple, List, Optional, Dict, Any
 
 from pygame import Rect, Surface
 from pygame.draw import rect, polygon
 from pygame.display import update
-
-from src.models.tile import Tile, State as TileState
+from pygame.font import Font
 
 from src.views.screen_view import ScreenView
 from src.views.button_view import ButtonView
 from src.views.tile_view import TileView
-from src.views.constant import WHITE, MAIN_BACKGROUND_COLOR, LARGE_PADDING, BUTTON_BACKGROUND_COLOR
+from src.views.constant import WHITE, MAIN_BACKGROUND_COLOR, LARGE_PADDING, \
+    BUTTON_BACKGROUND_COLOR, MAIN_FONT, MEDIUM_TEXT, RED_TILE, BLUE_TILE, BLACK
 
+from src.models.constant import Color
 from src.models.board import Board
+from src.models.player import Player
 
-
-class BoardViewButton(enum.Enum):
-    QUIT: int = 0,
-    UNDO: int = 1,
-    REDO: int = 2
-    HINT: int = 3
+from src.controllers.board_controller import BoardController
+from src.controllers.player_controller import PlayerController
 
 PARALLELOGRAM = {
     'TOP': (
@@ -62,20 +60,52 @@ PARALLELOGRAM = {
 }
 
 
-class BoardView(ScreenView):
-    
-    _quit_button: ButtonView
-    _undo_button: ButtonView
-    _redo_button: ButtonView 
-    _hint_button: ButtonView 
+class BoardViewButton(enum.Enum):
+    QUIT: int = 0,
+    UNDO: int = 1,
+    SWAP: int = 2
+    HINT: int = 3
+    TILE: int = 4
 
+
+class BoardView(ScreenView):
+
+    _buttons: List[ButtonView]
+    _tiles: List[TileView]
 
     _board: Board
 
-    def __init__(self, screen: Surface, size: int, board: Board):
+    _player_1: Player
+    _player_2: Player
+
+    _player_1_controller: PlayerController
+    _player_2_controller: PlayerController
+
+    _current_player: Player
+
+    def __init__(self,
+                 screen: Surface,
+                 size: int,
+                 board: Board,
+                 player_1: Player,
+                 player_2: Player):
         super().__init__(screen, size)
         
         self._board = board
+        
+        self._board_controller = BoardController(board)
+
+        self._player_1 = player_1
+        self._player_2 = player_2 
+
+        self._player_1_controller = PlayerController(player_1)
+        self._player_2_controller = PlayerController(player_2)
+
+        self._current_player = player_1
+
+    def start_2_player_game(self):
+        self._player_1_controller.human()
+        self._player_2_controller.human()
 
     def _draw_background(self):
         rect(
@@ -86,92 +116,134 @@ class BoardView(ScreenView):
             )
         )
 
+    def _button_views(self):
+        return [
+            {'ID': BoardViewButton.QUIT, 'TEXT': 'Quit', 'LOCATION':  (self._width - (65.5 + 50 - 27), self._height - 50), 'BACKGROUND': BUTTON_BACKGROUND_COLOR},
+            {'ID': BoardViewButton.SWAP, 'TEXT': 'Swap', 'LOCATION': (self._width - 355 - 5, self._height - 50), 'BACKGROUND': (153, 153, 255)},
+            {'ID': BoardViewButton.UNDO, 'TEXT': 'Undo', 'LOCATION': (self._width - 318.5 + 55, self._height - 50), 'BACKGROUND': (153, 153, 255)},
+            {'ID': BoardViewButton.HINT, 'TEXT': 'Hint', 'LOCATION': (self._width - 228.5 + 55, self._height - 50), 'BACKGROUND': (204, 153, 255)}
+        ]
+
+
     def _draw_buttons(self):
+        def _draw_button(button: Dict[str, Any]):
+            disabled = False
 
-        self._quit_button = ButtonView(
-            BoardViewButton.QUIT,
-            self._screen,
-            (self._width - (65.5 + 50), self._height - 50),
-            'Quit Game',
-            text_color=WHITE,
-            background_color=BUTTON_BACKGROUND_COLOR
-        )
-        # 131
+            if button['ID'] == BoardViewButton.SWAP:
+                disabled = self._board.number_moves() != 1 or self._board.is_swapped()
+            elif button['ID'] == BoardViewButton.UNDO:
+                disabled = self._board.number_moves() < 1 or not self._board.is_current() 
 
-        # (65.5 + 50 + 65.5 + 37.5 + 10 + 80 + 10 + 84 + 10)
-        self._undo_button = ButtonView(
-            BoardViewButton.UNDO,
-            self._screen,
-            (self._width - 412.5, self._height - 50),
-            'Undo',
-            text_color=WHITE,
-            background_color=(153, 153, 255)
-        )
+            return ButtonView(
+                button['ID'],
+                self._screen,
+                button['LOCATION'],
+                button['TEXT'],
+                text_color=WHITE,
+                background_color=button['BACKGROUND'],
+                disabled = disabled
+            )
 
-        # (65.5 + 50 + 65.5 + 37.5 + 10 + 80 + 10)
-        self._redo_button = ButtonView(
-            BoardViewButton.REDO,
-            self._screen,
-            (self._width - 318.5, self._height - 50),
-            'Redo',
-            text_color=WHITE,
-            background_color=(153, 153, 255)
-        )
-        # 81
+        self._buttons = [
+            _draw_button(button)
+            for button in self._button_views()
+        ]
 
-        # (65.5 + 50 + 65.5 + 37.5 + 10)
-        self._hint_button = ButtonView(
-            BoardViewButton.HINT,
+    def _draw_parallelogram(self):
+        for color, shape in PARALLELOGRAM.values():
+            polygon(
+                self._screen,
+                color,
+                shape           
+            )            
+
+    def _draw_tile(self, x_index, y_index):
+        x_coord = 90 + (17 * y_index) + (35 * x_index)
+        y_coord = 165 + (30 * y_index) 
+        return TileView(
             self._screen,
-            (self._width - 228.5, self._height - 50),
-            'Hint',
-            text_color=WHITE,
-            background_color=(204, 153, 255)
+            self._board.find_tile(x_index, y_index),
+            (x_coord, y_coord)
         )
-        # 75
 
     def _draw_tiles(self):
 
-        def _draw_parallelogram():
-            for color, shape in PARALLELOGRAM.values():
-                polygon(
-                    self._screen,
-                    color,
-                    shape           
-                )            
+        self._draw_parallelogram()
 
-        _draw_parallelogram()
+        self._tiles = [
+            [
+                self._draw_tile(x_index, y_index)
+                for x_index in range(0, self._board.size())
+            ]
+            for y_index in range(0, self._board.size())     
+        ]
 
-        for y_index in range(0, self._board.size()):
-            for x_index in range(0, self._board.size()):
-                x_coord = 90 + (17 * y_index) + (35 * x_index)
-                y_coord = 165 + (30 * y_index) 
-                TileView(
-                    self._screen,
-                    self._board.find_tile(x_index, y_index),
-                    (x_coord, y_coord)
-                )
+    def _draw_labels(self):
+        label: Font = Font(MAIN_FONT, MEDIUM_TEXT)
+        label_surface: Surface = label.render(
+            'Turn:',
+            True,
+            BLACK
+        )
+        label_rect: Rect = label_surface.get_rect()
+        label_rect.center = (36 + 50, self._height - 50)
 
-    def mouse_button_down(self, mouse: Tuple[int, int]) -> enum.Enum:
+        player_label: Font = Font(MAIN_FONT, MEDIUM_TEXT)
+        player_label_surface: Surface = player_label.render(
+            self._current_player.name(),
+            True,
+            RED_TILE if self._current_player.color() == Color.RED else BLUE_TILE
+        )
+        plauer_label_rect: Rect = player_label_surface.get_rect()
+        plauer_label_rect.center = (56 + 50 + 80, self._height - 50)
+
+        self._screen.blit(label_surface, label_rect)
+        self._screen.blit(player_label_surface, plauer_label_rect)
+
+
+    def _tile_clicked(self, mouse: Tuple[int, int]) -> bool:
+        for row in self._tiles:
+            for tile_view in row:
+                if tile_view.collidepoint(mouse) and tile_view.on_mouse_down(self._current_player):
+                    self._board_controller.append_history(tile_view.tile())
+                    return True
+        return False
+    
+    def _swap_first_tile(self):
+        self._board_controller.swap_first_tile(self._current_player)
+
+    def _change_current_player(self):
+        if self._current_player == self._player_1:
+            self._current_player = self._player_2
+        else:
+            self._current_player = self._player_1
+    
+    def on_mouse_down(self, mouse: Tuple[int, int]) -> enum.Enum:
         button_clicked = self._find_button(
             mouse,
-            [self._quit_button, self._hint_button, self._redo_button, self._undo_button]
+            self._buttons
         )
         
         if button_clicked == BoardViewButton.QUIT:
+            self._board_controller.reset()
             return button_clicked
-        elif button_clicked == BoardViewButton.REDO:
-            print("Redo Clicked")
+        elif button_clicked == BoardViewButton.SWAP:
+            self._swap_first_tile()
+            self._change_current_player()
         elif button_clicked == BoardViewButton.UNDO:
-            print("Undo Clicked")
+            self._board_controller.undo_history(self._current_player)
+            self._change_current_player()
         elif button_clicked == BoardViewButton.HINT:
             print("Hint Clicked")
         else:
-            print("Not Registered")
-        
+            if self._tile_clicked(mouse):
+                self._change_current_player()
+            else:
+                print("Not Registered")       
 
     def update(self):
         self._draw_background()
         self._draw_buttons()
         self._draw_tiles()
+        self._draw_labels()
         update()
