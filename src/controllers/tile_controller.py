1484ec4306bf7eed9ff5fe97746e92aa2fa67421
carlcastello <carl.castello96@gmail.com>
2020-11-23 15:43:00 -0700

    Reset, onclicked and update color methods

--- src/controllers/tile_controller.py
+++ src/controllers/tile_controller.py
@@ -1,4 +1,8 @@
+
+from src.models.constant import Color
 from src.models.tile import Tile
+from src.models.player import Player, Order
+
 
 class TileController:
     
@@ -7,4 +11,16 @@ class TileController:
     def __init__(self, tile: Tile):
         self._tile = tile
 
-    
+    def on_player_clicked(self, player: Player) -> bool:
+        if not self._tile.is_lock():
+            self._tile.update_color(player.color())
+            self._tile.lock_tile()
+            return True
+        return False
+
+    def reset(self):
+        self._tile.update_color(Color.WHITE)
+        self._tile.unlock_tile()
+
+    def update_color(self, color: Color):
+        self._tile.update_color(color)
