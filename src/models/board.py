6a92ee45c6ea2a8613e1dff10b539a84a93a9f3c
carlcastello <carl.castello96@gmail.com>
2020-11-23 15:46:18 -0700

    History and Swap variables

--- src/models/board.py
+++ src/models/board.py
@@ -1,15 +1,29 @@
-from typing import Tuple
+from typing import Tuple, List
 
 from src.models.tile import Tile
+from src.models.constant import Color
 
 class Board:
 
     _size: int
 
-    _matrix: Tuple[Tuple[Tile , ...], ...] = []
+    _matrix: Tuple[Tuple[Tile , ...], ...]
+
+    _is_swapped: bool = False
+    
+    _is_current: bool = True
+    _history: List[Tile] = []
+
 
     def __init__(self):
         self._size = 11
+
+        self.initialize_matrix()
+        self.initialize_history()
+
+
+    def initialize_matrix(self):
+        self._matrix = []
 
         def _neighbors(column_index, row_index):
             return [
@@ -32,6 +46,10 @@ class Board:
                 )
             self._matrix.append(row)
 
+    def initialize_history(self):
+        self._history = []
+
+
     def matrix(self) -> Tuple[Tuple[Tile , ...], ...]:
         return self._matrix
 
@@ -40,6 +58,36 @@ class Board:
 
     def find_tile(self, x, y) -> Tile:
         return self._matrix[y][x]
+
+    def is_swapped(self) -> bool:
+        return self._is_swapped
+
+    def swapped(self):
+        self._is_swapped = True
+    
+    def unswapped(self):
+        self._is_swapped = False
+
+    def is_current(self):
+        return self._is_current
+
+    def current(self):
+        self._is_current = True
+
+    def not_current(self):
+        self._is_current = False
+
+    def number_moves(self) -> int:
+        return len(self._history)
+
+    def last_move(self):
+        return self._history[-1]
+
+    def delete_last_move(self):
+        self._history = self._history[:-1]
+
+    def append_history(self, tile: Tile):
+        self._history.append(tile)
 
     def check_win(self):
         # TODO: Check the win condition
