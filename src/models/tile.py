c6cb57a798820f89eaa9d152f04e3cd5b66a38d3
carlcastello <carl.castello96@gmail.com>
2020-11-23 15:46:55 -0700

    lock and color update

--- src/models/tile.py
+++ src/models/tile.py
@@ -1,12 +1,6 @@
-import enum 
-
 from typing import Tuple, List
 
-
-class State(enum.Enum):
-    WHITE: int = 0
-    RED: int = 1
-    BLUE: int = 2
+from src.models.constant import Color
 
 class Tile:
 
@@ -14,12 +8,15 @@ class Tile:
 
     _neighbors: List[Tuple[int, int]]
 
-    _state: State = State.WHITE
+    _color: Color = Color.WHITE
+
+    _lock: bool
 
     def __init__(self, id: Tuple[int, int], neighbors: List[Tuple[str, int]]):
         self._id = id
         self._neighbors = neighbors
-        self._state = State.WHITE
+        self._color = Color.WHITE
+        self._lock = False
 
     def id(self) -> Tuple[int, int]:
         return self._id
@@ -27,11 +24,20 @@ class Tile:
     def neighbors(self) -> List[Tuple[str, int]]:
         return self._neighbors
 
-    def state(self) -> State:
-        return self._state
+    def color(self) -> Color:
+        return self._color
 
-    def update_state(self, new_state) -> None:
-        self.__state = new_state
+    def update_color(self, color) -> None:
+        self._color = color
+
+    def is_lock(self) -> bool:
+        return self._lock
+
+    def unlock_tile(self) -> None:
+        self._lock = False
+
+    def lock_tile(self) -> None:
+        self._lock = True
 
     def __str__(self):
         return f'{self._id[0]}{str(self._id[1])}'
