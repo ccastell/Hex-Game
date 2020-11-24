47398a06aa62b43c5914a0035a7158c49c5af7f5
carlcastello <carl.castello96@gmail.com>
2020-11-24 12:11:31 -0700

    A* algorithm to check for a win

--- src/controllers/board_controller.py
+++ src/controllers/board_controller.py
@@ -1,10 +1,39 @@
-
+from typing import List, Tuple
 
 from src.models.player import Player
 from src.models.board import Board
 from src.models.tile import Tile
+from src.models.constant import Color
 
 from src.controllers.tile_controller import TileController
+
+
+class Node:
+
+    _id: Tuple[int, int]
+
+    _g: int
+    _h: int
+    _f: int
+
+    def __init__(self, id:  Tuple[int, int], g: int, h: int):
+        self._id = id
+        self._g = g
+        self._h = h
+        self._f = g + h
+
+    def id(self) -> Tuple[int, int]:
+        return self._id
+
+    def g(self) -> int:
+        return self._g
+
+    def h(self) -> int:
+        return self._h
+    
+    def f(self) -> int:
+        return self._f
+
 
 class BoardController:
 
@@ -43,8 +72,46 @@ class BoardController:
             tile_controler.update_color(current_player.color())
             self._board.swapped()
 
+    # https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+    def check_win(self) -> bool:
+        last_tile: Tile = self._board.last_move()
+
+        edge_nodes: List[Node] = []
+
+        for direction in last_tile.heuristics().keys():
+
+            open_list: List[Node] = [
+                Node(last_tile.id(), 0, last_tile.heuristics()[direction])
+            ]
+            closed_list: List[Tuple[int, int]] = []
+
+            while len(open_list) > 0:
+                open_list.sort(key = lambda node: node.f())
+                current_node = open_list.pop()
+                closed_list.append(current_node.id())
+
+                if current_node.h() == 0:
+                    edge_nodes.append(current_node.id())
+
+                curent_tile: Tile =  self._board.find_tile(*current_node.id())
+                for neighbor in curent_tile.neighbors():
+                    tile: Tile =  self._board.find_tile(*neighbor)
+                    if tile.color() == curent_tile.color() and tile.id() not in closed_list:
+                        open_list.append(
+                            Node(tile.id(), current_node.g() + 1, tile.heuristics()[direction])
+                        )
+
+        index: int = 0 if last_tile.color() == Color.RED else 1
+        unique_indices: Tuple[int] = set(
+            [node[index] for node in edge_nodes if node[index] == 0 or node[index] == 10]
+        )
+
+        if len(unique_indices) >= 2:
+            self._board.set_game_over()
+            for row in self._board.matrix():
+                for tile in row:
+                    tile_controller: TileController = TileController(tile)
+                    tile_controller.lock()
+
     def reset(self):
-        self._board.initialize_matrix()
-        self._board.initialize_history()
-        self._board.unswapped()
-        self._board.current()
+        self._board.reset()
